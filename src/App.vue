7ce8c76417a84462b554e9d45a9cdf639bcc5ce6
Lucas Silva <lucassilvayoshi@gmail.com>
2024-05-02 13:55:09 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Calculadora from '../src/components/Calculadora.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Calculadora,
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <Calculadora />
+  </div>
+</template>
+
